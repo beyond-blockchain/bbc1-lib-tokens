00d448a8aa1a66e8d5b0bb13961e31648bf71bda
Kenji Saito <ks91@sfc.wide.ad.jp>
2018-06-13 05:52:01 +0900

    initial atomic swap implementation.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ bbc1_classifiers = [
 
 setup(
     name='bbc1-lib-tokens',
-    version='0.0.1',
+    version='0.10',
     description='Token library of Beyond Blockchain One',
     long_description=readme,
     url='https://github.com/beyond-blockchain/bbc1',
