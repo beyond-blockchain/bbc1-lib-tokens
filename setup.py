e0f4b78783b94c221c65a96e92514871460d93a9
Kenji Saito <ks91@sfc.wide.ad.jp>
2018-06-15 10:09:40 +0900

    version expression.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ bbc1_classifiers = [
 
 setup(
     name='bbc1-lib-tokens',
-    version='0.10',
+    version='0.10.0',
     description='Token library of Beyond Blockchain One',
     long_description=readme,
     url='https://github.com/beyond-blockchain/bbc1',
