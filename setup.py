e10a0e5428e933ef6aced67f74c25e1437c63b06
Kenji Saito <ks91@sfc.wide.ad.jp>
2019-06-26 10:58:33 +0900

    allows multiple instances of mints in a process.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ bbc1_classifiers = [
 
 setup(
     name='bbc1-lib-tokens',
-    version='0.12',
+    version='0.12.1',
     description='Token library of Beyond Blockchain One',
     long_description=readme,
     url='https://github.com/beyond-blockchain/bbc1-lib-tokens',
